9f53ab719e9c5f5e38dc19393b4573ed15b32c1e
Devon Martin <devmart10@gmail.com>
2019-12-24 22:02:36 -0800

    Customizes the layout of the blog.

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,73 +3,59 @@ import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
-
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
-    return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    )
+const Layout = ({ location, title, children }) => {
+  let container = {
+    marginLeft: `auto`,
+    marginRight: `auto`,
+    maxWidth: rhythm(24),
   }
+
+  let header = (
+    <h3
+      style={{
+        fontFamily: `Montserrat, sans-serif`,
+        marginTop: 0,
+        backgroundColor: `black`,
+        padding: rhythm(1),
+      }}
+    >
+      <div style={{ ...container }}>
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `white`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </div>
+    </h3>
+  )
+
+  let footer = (
+    <div
+      style={{
+        backgroundColor: `lightgrey`,
+        padding: rhythm(1),
+      }}
+    >
+      <div style={{ ...container }}>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </div>
+    </div>
+  )
+
+  return (
+    <div>
+      <header>{header}</header>
+      <main style={{ ...container }}>{children}</main>
+      <footer>{footer}</footer>
+    </div>
+  )
 }
 
 export default Layout
