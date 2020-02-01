fc43545d80947acbb40dff41d3502e666ddf406b
Devon Martin <devmart10@gmail.com>
2020-02-01 10:10:04 -0800

    begin adding real content to match figma

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+import Title from "../shared/components/Title"
 
 class AboutPage extends React.Component {
   render() {
@@ -14,18 +15,40 @@ class AboutPage extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="About" />
         <section>
-          <h1>Resume</h1>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <Title>About</Title>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut minus
+            cum eos rem cupiditate nihil illo assumenda aperiam maiores officia.
+            Expedita quidem quod magnam quaerat, incidunt officia praesentium
+            nobis sunt ex cum rerum eveniet natus veritatis perferendis alias
+            quam. Reiciendis vero amet aliquam excepturi rem ad provident
+            molestiae laboriosam optio et. Officiis aliquam possimus dicta nam
+            blanditiis natus accusamus tempora!
+          </p>
         </section>
-        <hr />
-        <Bio />
+        <section>
+          <Title>Resume</Title>
+          <h2>Summary of Qualifications</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut minus
+            cum eos rem cupiditate nihil illo assumenda aperiam maiores officia.
+            Expedita quidem quod magnam quaerat, incidunt officia praesentium
+            nobis sunt ex cum rerum eveniet natus veritatis perferendis alias
+            quam. Reiciendis vero amet aliquam excepturi rem ad provident
+            molestiae laboriosam optio et. Officiis aliquam possimus dicta nam
+            blanditiis natus accusamus tempora!
+          </p>
+          <h2>Work Experience</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            veritatis consectetur vero accusantium sed! Aliquid aperiam hic
+            provident rerum non, quo similique beatae in. Ea soluta quasi
+            nesciunt enim, molestias repellendus rerum sapiente consequatur modi
+            corrupti laudantium quam quod consectetur.
+          </p>
+        </section>
       </Layout>
     )
   }
